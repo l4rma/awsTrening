c9d29f0734a2cd905d2e25e980f79416d7a416e9
l4rma <lars.magelssen@gmail.com>
2021-10-19 10:24:06 +0200

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# awsTrening
+#DevOps Øving
+* [x] Lag spring boot app
+* [x] Github repo
+* [x] Dockerfile, build og run docker
+* [x] Pipeline med github actions (pakker og tester)
+* [ ] Legg til docker i workflow/github actions
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -9,5 +10,12 @@ class DemoApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void shouldBeFive() {
+		int fem = 5;
+		assertTrue(fem == 5);
+	}
+
 
 }
